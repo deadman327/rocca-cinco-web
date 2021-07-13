23f2e9420ff9db410e313c52cc4e6b0b914b2e76
azaelrock0 <azaelrock0@gmail.com>
2021-07-13 15:10:51 -0500

    Coneccion con MongoDB Atlas
    Coneccion con MongoDB Atlas

--- src/database.js
+++ src/database.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
 
-const { Rocca_cinco_MONGODB_HOST, Rocca_cinco_MONGODB_DATABASE} = process.env;
-const MONGODB_URI = `mongodb://${Rocca_cinco_MONGODB_HOST}/${Rocca_cinco_MONGODB_DATABASE}`;
+//const { Rocca_cinco_MONGODB_HOST, Rocca_cinco_MONGODB_DATABASE} = process.env;
+//const MONGODB_URI = `mongodb://${Rocca_cinco_MONGODB_HOST}/${Rocca_cinco_MONGODB_DATABASE}`;
+
+const { Rocca_cinco_user, Rocca_cinco_pass, Rocca_cinco_database} = process.env;
+const MONGODB_URI = `mongodb+srv://${Rocca_cinco_user}:${Rocca_cinco_pass}@cluster0.e6m3j.mongodb.net/${Rocca_cinco_database}?retryWrites=true&w=majority`;
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
